400988ae6dcddc48edfa2297dc55dce348b9fc2c
Somya Maheshwari <somyamaheshwari2006@gmail.com>
2026-06-06 11:39:15 +0530

    feat: add global mute button

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/theme.css'
-
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Analytics mode="production" />
   </React.StrictMode>,
 )
